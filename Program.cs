242adb39bf7c86ecc132719acdfa2edecad343e2
Benedykt Kościński <s17472@pjwstk.edu.pl>
2019-10-10 00:48:36 +0200

    Init project

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Klika
+{
+    public class Vertex
+    {
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new Dictionary<Vertex, List<int>>();
+            var result = new List<Vertex>();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
