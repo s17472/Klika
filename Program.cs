ae07f5985c17cc661ef9e697167d1a372a6cef51
Benedykt <bben234@gmail.com>
2019-10-10 14:55:43 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Klika
 {
@@ -10,6 +12,51 @@ namespace Klika
         public int Y { get; set; }
     }
 
+    public class AdjacencyMatrix
+    {
+        private BitArray[] _matrix;
+        public int Lenght { get; }
+
+        public AdjacencyMatrix(int lenght)
+        {
+            _matrix = new BitArray[lenght];
+            for(int i = 0; i < lenght;i++)
+            {
+                _matrix[i] = new BitArray(lenght); 
+            }
+
+            Lenght = lenght;
+        }
+
+        public bool IsConnected(int vertex1, int vertex2)
+        {
+            return _matrix[vertex1][vertex2];
+        }
+
+        public void SetConnection(int vertex1, int vertex2)
+        {
+            _matrix[vertex1][vertex2] = _matrix[vertex2][vertex1] = true;
+        }
+
+         public void RemoveConnection(int vertex1, int vertex2)
+        {
+            _matrix[vertex1][vertex2] = _matrix[vertex2][vertex1] = false;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach(var row in _matrix){
+                foreach(bool cell in row){
+                    sb.Append(cell ? 1 : 0);
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    } 
+
     class Program
     {
         static void Main(string[] args)
@@ -17,11 +64,15 @@ namespace Klika
             var graph = new Dictionary<Vertex, List<Vertex>>();
             var result = new List<Vertex>();
 
+            var xd = new AdjacencyMatrix(3);
+            xd.SetConnection(0,2);
 
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine(xd);
         }
 
-        public static float CheckAnswer(Dictionary<Vertex, List<Vertex>> graph, List<Vertex> answer)
+        public static float CheckAnswer(Dictionary<Vertex, List<Vertex>> graph, List<Vertex> answer, int cliqueLength)
         {
             return 1;
         }
