2401e6b6058dea42938924480c6d0aa085eebabb
Benedykt <s17472@pjwstk.edu.pl>
2020-01-09 13:18:11 +0100

    Update cover.py

--- cover.py
+++ cover.py
@@ -1,5 +1,4 @@
 import itertools as it
-import time
 from math import factorial
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,10 +6,9 @@ import random as rm
 from xlwings import xrange
 import json
 import collections
-import pickle
 import sys
-from tqdm import tqdm
 import timeit
+from tqdm import tqdm
 
 time_units = {'ms': 1, 's': 1000, 'm': 60 * 1000, 'h': 3600 * 1000}
 
@@ -36,8 +34,8 @@ class CodeTimer:
         self.took = self.took / time_units.get(self.unit, time_units['ms'])
 
         if not self.silent:
-            log_str = 'Code block{}took: {:.5f} {}'.format(
-                str(" '" + self.name + "' ") if self.name else ' ',
+            log_str = '{}took: {:.5f} {}'.format(
+                str(self.name + " ") if self.name else '',
                 float(self.took),
                 str(self.unit))
 
@@ -47,7 +45,7 @@ class CodeTimer:
                 print(log_str)
 
 
-#region Json
+# region Json
 class JSONSetEncoder(json.JSONEncoder):
     """Use with json.dumps to allow Python sets to be encoded to JSON
 
@@ -73,6 +71,7 @@ class JSONSetEncoder(json.JSONEncoder):
         else:
             return json.JSONEncoder.default(self, obj)
 
+
 def json_as_python_set(dct):
     """Decode json {'_set_object': [1,2,3]} to set([1,2,3])
 
@@ -86,20 +85,55 @@ def json_as_python_set(dct):
     if '_set_object' in dct:
         return set(dct['_set_object'])
     return dct
-#endregion
+# endregion
 
 
+class TestData:
+    def __init__(self, universe, sets, iterations, tabu_size, n_size):
+        self.universe = universe
+        self.sets = sets
+        self.iterations = iterations
+        self.tabu_size = tabu_size
+        self.n_size = n_size
+
+    @staticmethod
+    def load_test_data(name):
+        file = []
+        with open(name, "r+") as text_file:
+            file = text_file.read().splitlines()
+        universe = [int(x) for x in file[0].split(',')]
+        sets = json.loads(file[1], object_hook=json_as_python_set)
+        iterations = int(file[2])
+        tabu_size = int(file[3])
+        n_size = int(file[4])
+        return TestData(universe, sets, iterations, tabu_size, n_size)
 
 
-def save_sets(S):
-    with open("Output.txt", "w") as text_file:
+def save_sets(S, name="Output.txt"):
+    with open(name, "w") as text_file:
         serialized = json.dumps(S, cls=JSONSetEncoder)
         text_file.write(serialized)
 
 
-def load_sets():
-    with open("Output.txt", "r+") as text_file:
+def load_sets(name="Output.txt"):
+    with open(name, "r+") as text_file:
         return json.loads(text_file.read(), object_hook=json_as_python_set)
+
+
+
+
+
+def init_test_data_files():
+    save_sets(generate_sets(U, 10), "10.txt")
+    save_sets(generate_sets(U, 12), "12.txt")
+    save_sets(generate_sets(U, 14), "14.txt")
+    save_sets(generate_sets(U, 16), "16.txt")
+    save_sets(generate_sets(U, 20), "20.txt")
+
+
+def load_test_data_files():
+    return {'10': load_sets("10.txt"), '12': load_sets("12.txt"), '14': load_sets("14.txt"),
+            '16': load_sets("16.txt"), '20': load_sets("20.txt")}
 
 
 def goal(s):
@@ -116,23 +150,22 @@ def get_combinations(s):
 
 
 def generate_sets(U, S_size):
-    rm.seed(seed)
     combination_count = get_combination_count(len(U))
     if combination_count > sys.maxsize:
-        raise Exception("U len is over 63")
-        
-    # print(combination_count)
-    combos = rm.sample(xrange(1, int(combination_count+1)), S_size)
-    # print(combos)
-    return [set(get_nth_combination(U, i)) for i in combos]
+        raise Exception(f"combination_count is over {sys.maxsize}")
 
+    # print(combination_count)
+    combos = rm.sample(xrange(1, int(combination_count + 1)), S_size)
+    # print(combos)
+    ret = [set(get_nth_combination(U, i)) for i in combos]
+    return ret
 
 
 def get_nth_combination(s, n):
-    combination_count = get_combination_count(len(U2))
+    combination_count = get_combination_count(len(s))
     if not (0 < n <= combination_count):
         n = int(n % combination_count)
-        print(f"n is <= 0 or exceeds possible combinations count, n = {n}")
+        # print(f"n is <= 0 or exceeds possible combinations count, n = {n}")
 
     bin_str = bin(n)[:1:-1]
     combo = []
@@ -157,7 +190,7 @@ def get_neighbourhood_close(s, start):
 
 def get_neighbourhood(s, start, n):
     ret = {}
-    for i in [x for x in range(start-n, start+n+1) if x != start]:
+    for i in [x for x in range(start - n, start + n + 1) if x != start]:
         ret[i] = goal(get_nth_combination(s, i))
     return ret
 
@@ -183,15 +216,14 @@ def brute(s):
     return [best_score, get_nth_combination(s, best_index)]
 
 
-def hill_full(s, x, n_size):
-    rm.seed(seed)
-    start = rm.randint(0, len(s)+1)
+def hill_full(s, it, n_size):
+    start = rm.randint(0, get_combination_count(len(s)) + 1)
     start_combo = get_nth_combination(s, start)
     best_score = goal(start_combo)
     best_combo = start
     # print(f"{best_score}: {get_nth_combination(s, best_combo)} : {best_combo}")
 
-    for i in (range(x)):
+    for i in (range(it)):
         n = get_neighbourhood(s, start, n_size)
         best_n_index = max(n, key=n.get)
         best_n_score = n[best_n_index]
@@ -206,15 +238,15 @@ def hill_full(s, x, n_size):
     # return [best_score]
 
 
-def hill_random(s, x, n_size):
-    rm.seed(seed)
-    start = rm.randint(0, len(s)+1)
+def hill_random(s, it, n_size):
+    n_size = n_size // 3
+    start = rm.randint(0, get_combination_count(len(s)) + 1)
     start_combo = get_nth_combination(s, start)
     best_score = goal(start_combo)
     best_combo = start
     # print(f"{best_score}: {get_nth_combination(s, best_combo)} : {best_combo}")
 
-    for i in (range(x)):
+    for i in (range(it)):
         n = get_neighbourhood(s, start, n_size)
         best_n_index = rm.choice(list(n.keys()))
         best_n_score = n[best_n_index]
@@ -225,20 +257,18 @@ def hill_random(s, x, n_size):
         start = best_n_index
         # print(f"{best_score}: {get_nth_combination(s, best_combo)} : {best_combo}")
 
-
     return [best_score, get_nth_combination(s, best_combo)]
     # return [best_score]
 
 
-def tabu(s, it, t_MAX, n_size):
-    rm.seed(seed)
-    start = rm.randint(0, len(s)+1)
+def tabu(s, it, t_size, n_size):
+    start = rm.randint(0, get_combination_count(len(s)) + 1)
     start_combo = get_nth_combination(s, start)
     best_score = goal(start_combo)
     best_combo = start
     checked = [start]
 
-    for i in (range(it)):
+    for i in tqdm(range(it)):
         n = get_neighbourhood(s, start, n_size)
         n_tabu = {key: value for (key, value) in n.items() if key not in checked}
         if not n_tabu:
@@ -251,30 +281,32 @@ def tabu(s, it, t_MAX, n_size):
         if best_score <= best_n_score:
             best_score = best_n_score
             best_combo = best_n_index
-        if len(checked) >= t_MAX:
+        if len(checked) >= t_size:
             break
         # print(f"B: {best_index}: {best_score}: {get_nth_combination(s, best_index)}")
 
-        time.sleep(1)
     return [best_score, get_nth_combination(s, best_combo)]
     # return [best_score]
 
 
-def benchmark(func):
-    print(func)
+def benchmark(fun, name):
+    start = timeit.default_timer()
+    print(fun())
+    took = (timeit.default_timer() - start) * 1000.0
+    took = took / time_units["ms"]
+    print("{:.5f}".format(took))
 
 
-S = [{2, 3}, {5}, {3, 4}, {4, 5}, {1, 2, 3}]
 S2 = [{0, 3, 5}, {0, 1, 4}, {1, 4, 5}, {1, 2}, {0, 3}]
 U = [1, 2, 3, 4, 5]
-seed = 441
-print(seed)
-U2 = range(10)
-k = 10
-S2 = generate_sets(U2, 500)
-with CodeTimer():
-    tabu(S2, k, 1000, 8)
-with CodeTimer():
-    tabu(S2, k, 1000, 8)
-benchmark(hill_full(S2, k, 8))
-benchmark(hill_random(S2, k, 2))
+k = 1500
+test2 = TestData.load_test_data('10.txt')
+init_test_data_files()
+xd = load_test_data_files()
+
+
+test = TestData(U, xd['10'], 500, 600, 4)
+benchmark(lambda: tabu(S2, k, 1500, 8), "tabu")
+benchmark(lambda: tabu(S2, k, 1000, 8), "tabu")
+benchmark(lambda: hill_full(S2, k, 8), "hill_full")
+benchmark(lambda: hill_random(S2, k, 2), "hill_random")
